af6da4373687e342ff8cce4e6a4d8f88d305d17c
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2023-11-11 16:18:05 +0100

    añadiendo nuevos docs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+ // He creado una constante agregar desde el id agregar
+ const agregar= document.getElementById("agregar");
+
+ // He creado un elemento Li
+ const lista= document.getElementById("lista")
+
+
+agregar.addEventListener('click', function () {
+    const nuevoElemento=prompt("inserta un elemento, este elemento se transformará en una lista");
+    if(nuevoElemento){
+    const elementoLista= document.createElement("li")
+        elementoLista.innerHTML=nuevoElemento;
+        lista.appendChild(elementoLista);
+    }
+  });
